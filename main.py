ec668c7dde390c79d81ee5628f6c33c3726d8acf
OmarIdrisT <146827333+OmarIdrisT@users.noreply.github.com>
2024-10-28 16:44:07 +0100

    Acabat

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+x = 2
+y = 2
 
+def on_forever():
+    led.plot_bar_graph(input.temperature(), 50)
+    global x,y 
+    led.plot(x, y)
+    basic.pause(50)
+    led.unplot(x, y)
+    acc_x = input.acceleration(Dimension.X)
+    acc_y = input.acceleration(Dimension.Y)
+
+    if acc_x <= 150 and x > 0:
+        x = x - 1
+    elif acc_x > 150 and x < 4:
+        x += 1
+
+    if acc_y <= 150 and y > 0:
+        y = y - 1
+    elif acc_y > 150 and y < 4:
+        y += 1
+
+
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+let x = 2
+let y = 2
+basic.forever(function on_forever() {
+    led.plotBarGraph(input.temperature(), 50)
+    
+    led.plot(x, y)
+    basic.pause(50)
+    led.unplot(x, y)
+    let acc_x = input.acceleration(Dimension.X)
+    let acc_y = input.acceleration(Dimension.Y)
+    if (acc_x <= 150 && x > 0) {
+        x = x - 1
+    } else if (acc_x > 150 && x < 4) {
+        x += 1
+    }
+    
+    if (acc_y <= 150 && y > 0) {
+        y = y - 1
+    } else if (acc_y > 150 && y < 4) {
+        y += 1
+    }
+    
+})
